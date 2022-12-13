3c8f1f9fb0abbfdb4bc9bc193e381c21bbdf8b1d
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2022-12-13 15:01:17 +0530

    Change color of text animation

--- Text_Animation.js
+++ Text_Animation.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Tawfeek Khan', 'Developer', 'Designer',''], 'text',['tomato','rebeccapurple','lightblue']);
+ consoleText(['Tawfeek Khan', 'Developer', 'Designer',''], 'text',['red','yellow','blue']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
