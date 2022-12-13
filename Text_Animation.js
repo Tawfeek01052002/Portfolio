85711179ba193464bdb79fb2283952e8f1cb8ce0
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2022-12-13 12:04:00 +0530

    Slightly MOdified

--- Text_Animation.js
+++ Text_Animation.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text',['tomato','rebeccapurple','lightblue']);
+ consoleText(['Tawfeek Khan', 'Web Developer', 'Designer',''], 'text',['white','ehite','white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
