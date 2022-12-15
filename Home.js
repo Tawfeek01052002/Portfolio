4defa048e1b53066c744cc9e33cc0cc9acc88a09
Tawfeek Parwez Khan <62948072+Tawfeek01052002@users.noreply.github.com>
2022-12-15 09:27:56 +0530

    second last update

--- Home.js
+++ Home.js
@@ -1,10 +1,15 @@
-$(document).ready(function () {
-    $(document).on('click', ".lines-button", function () {
-        $('#overlay').show();
-        $('.lines-button').addClass('close');
-    });
-    $(document).on('click', ".lines-button.close", function () {
-        $('#overlay').hide();
-        $('.lines-button').removeClass('close');
-    });
-});
+var myNav = document.getElementById('nav');
+var mylogo=document.querySelector('.logo_1');
+window.onscroll = function () { 
+    console.log(document.documentElement.scrollTop);
+    if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50 ){
+        myNav.classList.add("nav-colored");
+        mylogo.classList.add("logo_1_color");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+        mylogo.classList.remove("logo_1_color");
+    }
+};
